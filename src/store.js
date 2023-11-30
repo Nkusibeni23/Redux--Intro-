9280e4c56f841bd5fa598ebd6441b585250d4971
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-30 18:31:40 +0200

    Working with action creators useReducers

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -20,9 +22,10 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) return state;
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
         // LATER
-        // loanPurpose: action.payload.loanPurpose,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
       };
     case "PAY_LOAN":
       return {
@@ -35,3 +38,45 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+// store.dispatch({ type: "DEPOSIT", payload: 500 });
+// store.dispatch({ type: "WITHDRAWAL", payload: 100 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "REQUEST_LOAN",
+//   payload: {
+//     amount: 1000,
+//     loanPurpose: "car",
+//   },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: "PAY_LOAN" });
+// console.log(store.getState());
+
+// const ACCOUNT_DEPOSIT = "DEPOSIT";
+
+function Deposit(amount) {
+  return { type: "DEPOSIT", payload: amount };
+}
+function Withdraw(amount) {
+  return { type: "WITHDRAWAL", payload: amount };
+}
+
+function Request_Loan(amount, loanPurpose) {
+  return { type: "REQUEST_LOAN", payload: { amount, loanPurpose } };
+}
+
+function Pay_Loan() {
+  return { type: "PAY_LOAN" };
+}
+
+store.dispatch(Deposit(500));
+console.log(store.getState());
+store.dispatch(Withdraw(100));
+console.log(store.getState());
+store.dispatch(Request_Loan(1000, "car"));
+console.log(store.getState());
+store.dispatch(Pay_Loan());
+console.log(store.getState());
